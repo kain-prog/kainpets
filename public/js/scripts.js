c67e615e1187ec2a71c474e9b1891c3b56069010
Matheus Santos <97212094+kain-prog@users.noreply.github.com>
2022-04-30 16:17:20 -0300

    Update scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -8,3 +8,39 @@
         } 
     });  
   });
+
+
+// Products appears with input select
+  
+
+let all = document.getElementById('all')
+let dogs = document.getElementById('dogs')
+let cats = document.getElementById('cats')
+
+let paizao = document.getElementById('paizao')
+
+
+const selecter = document.getElementById('opt')
+
+selecter.addEventListener("change",function (e){
+  console.log(e.target.value);
+  if (e.target.value == 1 ) {
+      paizao.style.opacity = 1;
+      all.style.display = "flex";
+      dogs.style.display = 'none';
+      cats.style.display = 'none'; 
+  }
+  if (e.target.value == 2) {
+      paizao.style.opacity = 1;
+      all.style.display = "none"
+      dogs.style.display = "flex";
+      cats.style.display = "none";
+  }
+  if (e.target.value == 3) {
+      paizao.style.opacity = 1;
+      all.style.display = "none";
+      dogs.style.display = "none";
+      cats.style.display = "flex";
+  }
+
+});
